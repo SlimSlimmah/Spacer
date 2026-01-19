09e0a6ae59b9bfc9a62000c3402ade412fb8c5a7
SlimSlimmah <richrobinson@hotmail.ca>
2026-01-18 23:57:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // src/main.js
 import * as Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3/dist/phaser.esm.js'
-
-
 import GameScene from './scenes/GameScene.js'
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
   backgroundColor: '#0b1020',
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
   physics: {
     default: 'arcade',
     arcade: {

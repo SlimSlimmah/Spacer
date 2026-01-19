c0a4c3b90a01b1ad139cbbd3868499f46e073c68
SlimSlimmah <richrobinson@hotmail.ca>
2026-01-18 21:47:37 -0500

    Add files via upload

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,1 +1,19 @@
+// src/scenes/GameScene.js
+import BasePlanet from '../entities/BasePlanet.js'
 
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('GameScene')
+  }
+
+  create() {
+    const cx = this.scale.width / 2
+    const cy = this.scale.height / 2
+
+    this.basePlanet = new BasePlanet(this, cx, cy)
+  }
+
+  update() {
+    this.basePlanet.update()
+  }
+}
